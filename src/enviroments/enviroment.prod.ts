27a35ed6a6af8879dc8d4ff5eed76f4db5ad986d
Rawda-ayman <rawdaashrey@gmail.com>
2025-10-26 17:32:45 +0300

    Update appurl to new production URL

--- src/enviroments/enviroment.prod.ts
+++ src/enviroments/enviroment.prod.ts
@@ -1,4 +1,4 @@
 export const enviroment = {
     baseurl:"https://ecommerce.routemisr.com/api/v1/" ,
-     appurl:"https://ecommerce.routemisr.com"
+     appurl:"https://e-commerce-lilac-omega-45.vercel.app/"
 }
